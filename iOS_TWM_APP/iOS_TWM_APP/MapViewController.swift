c0f2b623a49b90f1f48a0cfb9341d387443ea8e1
Sting Hsieh <sting20210@gmail.com>
2024-08-23 15:57:52 +0800

    Bottom menu push without package

---
+++ iOS_TWM_APP/iOS_TWM_APP/MapViewController.swift
@@ -0,0 +1,234 @@
+//
+//  MapViewController.swift
+//  iOS_TWM_APP
+//
+//  Created by 謝霆 on 2024/8/23.
+//
+
+import UIKit
+
+import SnapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        configMapView()
+        
+        configBottomMenuView()
+        
+        customizeLabels()
+        
+    }
+    
+    
+    let mapView = UIView()
+    
+    let bottomMenuView = UIView()
+    
+    var isExpanded: Bool? = false
+    
+    let recentUpdateLabel = UILabel()
+    
+    let timeLabel = UILabel()
+    
+    let dateLabel = UILabel()
+    
+    let deviceNameLabel = UILabel()
+    
+    let stepCountLabel = UILabel()
+    
+    let frequencyLabel = UILabel()
+    
+    let frequencyValueLabel = UILabel()
+    
+    let stepCountTextLabel = UILabel()
+    
+    let stepCountValueLabel = UILabel()
+    
+    let refreshButton = UIButton()
+    
+    let locateButton = UIButton()
+    
+    let searchButton = UIButton()
+    
+    func configMapView() {
+        
+        view.addSubview(mapView)
+        
+        mapView.snp.makeConstraints { make in
+            
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+            
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right)
+            
+        }
+        
+        mapView.backgroundColor = .systemGray5
+        
+    }
+    
+    func configBottomMenuView() {
+        
+        view.addSubview(bottomMenuView)
+        
+        [recentUpdateLabel, timeLabel, dateLabel, deviceNameLabel, stepCountTextLabel,
+         stepCountValueLabel, frequencyLabel, frequencyValueLabel].forEach {
+            bottomMenuView.addSubview($0)
+        }
+        
+        bottomMenuView.snp.makeConstraints { make in
+            
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(670)
+            
+            make.bottom.equalTo(view)
+            
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+            
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.right)
+            
+        }
+        
+        bottomMenuView.layer.cornerRadius = 15
+        
+        bottomMenuView.backgroundColor = .systemGray3
+        
+        let tapBottomMenuGesture = UITapGestureRecognizer(target: self, action: #selector(didTappedBottomView))
+        
+        bottomMenuView.addGestureRecognizer(tapBottomMenuGesture)
+        
+        setBottomViewConstraint()
+        
+    }
+    
+    func setBottomViewConstraint() {
+        
+        let commonYAnchor = deviceNameLabel.snp.bottom
+
+                
+        recentUpdateLabel.snp.makeConstraints { make in
+            make.top.equalTo(commonYAnchor).offset(8)
+            make.left.equalTo(bottomMenuView.snp.left).offset(40)
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(recentUpdateLabel.snp.bottom).offset(8)
+            make.left.equalTo(dateLabel.snp.right).offset(4)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(recentUpdateLabel.snp.bottom).offset(18)
+            make.left.equalTo(bottomMenuView.snp.left).offset(12)
+        }
+        
+        deviceNameLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(bottomMenuView.snp.top).offset(25)
+            make.centerX.equalTo(bottomMenuView.snp.centerX)
+        }
+        
+        stepCountTextLabel.snp.makeConstraints { make in
+            make.top.equalTo(deviceNameLabel.snp.bottom).offset(18)
+            make.centerX.equalTo(deviceNameLabel.snp.centerX)
+        }
+        
+        stepCountValueLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(stepCountTextLabel.snp.centerX)
+            make.top.equalTo(stepCountTextLabel.snp.bottom).offset(8)
+        }
+        
+        frequencyLabel.snp.makeConstraints { make in
+            make.top.equalTo(commonYAnchor).offset(8) // Aligning Y with recentUpdateLabel
+            make.right.equalTo(bottomMenuView.snp.centerX).offset(155)
+        }
+        
+        frequencyValueLabel.snp.makeConstraints { make in
+            make.top.equalTo(frequencyLabel.snp.bottom).offset(8)
+            make.centerX.equalTo(bottomMenuView.snp.centerX).offset(140)
+        }
+        
+        stepCountTextLabel.snp.makeConstraints { make in
+            make.top.equalTo(commonYAnchor).offset(8) // Aligning Y with recentUpdateLabel
+            make.centerX.equalTo(deviceNameLabel.snp.centerX)
+        }
+        
+    }
+    
+    func addBottonMenuButtons() {
+        
+        self.bottomMenuView.addSubview(searchButton)
+        
+        self.bottomMenuView.addSubview(locateButton)
+
+        self.bottomMenuView.addSubview(refreshButton)
+        
+        
+
+        
+    }
+    
+    func customizeLabels() {
+        recentUpdateLabel.text = "最近更新"
+        timeLabel.text = "08:08"
+        dateLabel.text = "01/01"
+        deviceNameLabel.text = "DeviceName"
+        stepCountTextLabel.text = "今日步數"
+        stepCountValueLabel.text = "0"
+        frequencyLabel.text = "頻率"
+        frequencyValueLabel.text = "一般"
+        
+        recentUpdateLabel.textColor = .systemGray
+        
+        stepCountTextLabel.textColor = .systemGray
+        
+        frequencyLabel.textColor = .systemGray
+        
+        // Customize fonts, colors, and alignment as needed
+        recentUpdateLabel.font = .systemFont(ofSize: 14)
+        stepCountTextLabel.font = .systemFont(ofSize: 14)
+        frequencyLabel.font = .systemFont(ofSize: 14)
+        timeLabel.font = .systemFont(ofSize: 24)
+        stepCountValueLabel.font = .systemFont(ofSize: 24)
+        frequencyValueLabel.font = .systemFont(ofSize: 24)
+        deviceNameLabel.font = .systemFont(ofSize: 24)
+        dateLabel.font = .systemFont(ofSize: 12)
+       
+    }
+    
+        
+        @objc func didTappedBottomView() {
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                if self.isExpanded == false {
+                    self.bottomMenuView.snp.updateConstraints { make in
+                        make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(600)
+                    }
+                    self.isExpanded = true
+                } else {
+                    self.bottomMenuView.snp.updateConstraints { make in
+                        make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(670)
+                    }
+                    self.isExpanded = false
+                }
+                self.view.layoutIfNeeded()  // Apply the constraint changes
+            })
+            
+            configExtendedText()
+        }
+    
+    func configExtendedText() {
+        
+
+        
+        
+        
+    }
+        
+    
+
+}
