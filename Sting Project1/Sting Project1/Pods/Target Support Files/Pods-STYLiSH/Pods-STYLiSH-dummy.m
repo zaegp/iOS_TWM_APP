6ef0379299f867b9f8e145578a50dbc433110866
Sting Hsieh <161547903+HsiehTing@users.noreply.github.com>
2024-08-26 16:31:46 +0800

    Delete Sting Project1 directory

--- Sting Project1/Sting Project1/Pods/Target Support Files/Pods-STYLiSH/Pods-STYLiSH-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_STYLiSH : NSObject
-@end
-@implementation PodsDummy_Pods_STYLiSH
-@end
--- Sting Project1/Sting Project1/STYLiSH/ViewController/addToCartTableView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  addToCartTableView.swift
-//  STYLiSH
-//
-//  Created by 謝霆 on 2024/7/29.
-//
-
-
